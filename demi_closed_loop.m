bbf27e03b360b2767b54281da45aec707f62cdee
Juliette Dutheil <juliettedutheil@gmail.com>
2021-05-03 19:52:33 +0100

    modif closed loop

--- demi_closed_loop.m
+++ demi_closed_loop.m
@@ -1,7 +1,39 @@
-function [outputArg1,outputArg2] = untitled(inputArg1,inputArg2)
-%UNTITLED Summary of this function goes here
+function [output]=demi_closed_loop(x_ref,y_ref,theta_ref)
+%DEMI_CLOSED_LOOP Closed loop without the guidance block
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+close all;
+x=0; y=0; theta=0;
+x_mem=[x]; y_mem=[y]; theta_mem=[theta]; v_mem=[0]; ws_mem=[0];
+time=0;
+for i=1:500
+    
+    u=control_block1(x_ref,y_ref,theta_ref,x,y,theta);
+    v=u(1);
+    ws=u(2);
+    v_mem=[v_mem;v];
+    ws_mem=[ws_mem;ws];
+    C=simulator(x,y,theta,v,ws,1,time);
+    x=C(1);
+    y=C(2);
+    theta=C(3);
+    x_mem=[x_mem;x];
+    y_mem=[y_mem;y];
+    theta_mem=[theta_mem;theta];
+    figure(1);
+    plot(x_mem(i),y_mem(i),'r+'); xlabel('x'); ylabel('y');
+    hold on;
+    figure(2);
+    plot(i, theta_mem(i),'b+'); xlabel('Time 0.01s'); ylabel('theta');
+    hold on;
+    figure(3);
+    plot(i,v,'g+');
+    hold on;
+    plot(i,ws,'y*');xlabel('Time 0.01s'); ylabel('V,ws');
+    hold on;
+    
+    time=time+i;
 end
+
+end
+
 

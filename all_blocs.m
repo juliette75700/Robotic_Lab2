35f3ae89c81df696fce2bd0f44729d44f8a67279
Juliette Dutheil <juliettedutheil@gmail.com>
2021-05-05 10:38:29 +0100

    Add control_bloc2

--- all_blocs.m
+++ all_blocs.m
@@ -12,21 +12,32 @@ plot(x_ref,y_ref,'b.');
 hold on;
 theta_ref=coord(3,:);
 time=0;
-h=0.01;
+h=1;
 p=length(coord);
 k=2;
 x=x_ref(1); y=y_ref(1); theta=theta_ref(1); %starting at same position as the ref
+x_deltat=x; y_deltat=y; theta_deltat=theta;
 x_mem=[x]; y_mem=[y]; theta_mem=[theta];v_mem=[0]; ws_mem=[0];% for plots later
-while k<=p
-    [v,ws]=control_bloc1(x_ref(k),y_ref(k),theta_ref(k),x,y,theta);
-    
+while k<=p    
+    [v,ws]=control_bloc2(x_ref(k),y_ref(k),theta_ref(k),x_deltat,y_deltat,theta_deltat,x,y,theta,10*h);
+    [x,y,theta]=simulator(x,y,theta,v,ws,h,time);
+    [x_deltat,y_deltat,theta_deltat]=simulator(x,y,theta,v,ws,7*h,time);
     v_mem=[v_mem;v];ws_mem=[ws_mem;ws];
-    
-    [x,y,theta]=simulator(x,y,theta,v,ws,1,time);
-    
     x_mem=[x_mem;x];y_mem=[y_mem;y];theta_mem=[theta_mem;theta];
+    
     time=time+h;
-    k=next_step(x,y,x_ref,y_ref);% compute the index of coord of the next step
+    if mod(time,10)==0
+        k=next_step(x,y,x_ref,y_ref);% compute the index of coord of the next step
+    end
+%     [v,ws]=control_bloc1(x_ref(k),y_ref(k),theta_ref(k),x,y,theta);
+%     v_mem=[v_mem;v];ws_mem=[ws_mem;ws];
+%     [x,y,theta]=simulator(x,y,theta,v,ws,h,time);
+%     x_mem=[x_mem;x];y_mem=[y_mem;y];theta_mem=[theta_mem;theta];
+%     
+%     time=time+h;
+%     if mod(time,10)==0
+%         k=next_step(x,y,x_ref,y_ref);% compute the index of coord of the next step
+%     end
 
 end
 figure(1);
